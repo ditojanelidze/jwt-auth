f78ccbabd997c8c2356a91adf3042404cbc29ae4
ditojanelidze <ditojanelidze28@gmail.com>
2020-05-07 21:45:37 +0400

    Fix style

--- api/services/token_storage.go
+++ api/services/token_storage.go
@@ -12,27 +12,27 @@ import (
 )
 
 var (
-	EXPIRATION = time.Hour * 2
+	EXPIRATION     = time.Hour * 2
 	REFRESH_LENGTH = 20
 )
 
-func RetrieveTokens(user models.User) (map[string]string, error){
+func RetrieveTokens(user models.User) (map[string]string, error) {
 	tokenString, err := generateAccessToken(user.ID)
-	if err != nil{
+	if err != nil {
 		return map[string]string{}, err
 	}
 	refreshToken := uniuri.NewLen(REFRESH_LENGTH)
-	tokens := map[string]string {
-		"access_token": tokenString,
+	tokens := map[string]string{
+		"access_token":  tokenString,
 		"refresh_token": refreshToken,
 	}
 	storeInRedis(user, tokens)
 	return tokens, err
 }
 
-func generateAccessToken(id int64) (string, error){
+func generateAccessToken(id int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user_id": id,
+		"user_id":  id,
 		"exp_date": time.Now().Add(time.Minute * 20),
 	})
 	return token.SignedString([]byte(os.Getenv("API_SECRET_KEY")))
@@ -46,8 +46,8 @@ func storeInRedis(user models.User, tokens map[string]string) {
 
 func redisClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%s",initializers.REDIS_URL, initializers.REDIS_PORT),
+		Addr:     fmt.Sprintf("%s:%s", initializers.REDIS_URL, initializers.REDIS_PORT),
 		Password: "",
-		DB: initializers.REDIS_DB,
+		DB:       initializers.REDIS_DB,
 	})
 }
